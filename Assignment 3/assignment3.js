64d60ac27b8a6898d171103d7942cddc641e1863
vickygit-hub <vicky.singh.250803@gmail.com>
2025-11-08 12:32:08 +0530

    50% killer restaurant template uploaded

---
+++ Assignment 3/assignment3.js
@@ -0,0 +1,39 @@
+// Select all boxes
+const redbox = document.getElementById('Red-box');
+const bluebox = document.getElementById('Blue-box');
+const greenbox = document.getElementById('Green-box');
+const yellowbox = document.getElementById('Yellow-box');
+
+// Add event listeners
+redbox.addEventListener('click', () => {
+  redbox.style.backgroundColor = 'red';
+});
+
+bluebox.addEventListener('click', () => {
+  bluebox.style.backgroundColor = 'blue';
+});
+
+greenbox.addEventListener('click', () => {
+  greenbox.style.backgroundColor = 'green';
+});
+
+yellowbox.addEventListener('click', () => {
+  yellowbox.style.backgroundColor = 'yellow';
+});
+
+document.querySelectorAll('.box').forEach(box =>{ 
+  box.addEventListener('dblclick', () => {
+    box.style.backgroundColor = 'white';
+  })
+})
+
+const header = document.querySelector('.header h2');
+
+const form = document.getElementById('greet-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // Prevent form submission
+  const nameInput = document.getElementById('name');
+  const name = nameInput.value;
+  header.textContent = `Hello, ${name}!`;
+  nameInput.value = ''; // Clear input field
+})
